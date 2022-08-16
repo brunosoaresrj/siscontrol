05b9d17d2fb951fd88ae6ae09567d74445d5ac37
Bruno Soares <bruno.soares.rj@gmail.com>
2022-08-16 14:37:44 -0300

    primeiro commit do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotebookComponent } from './notebook/notebook.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotebookComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
